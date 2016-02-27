0b55ca5035b194a6c7e6b0b6b7bb7e02c8287e80
fo40225 <fo40225@hotmail.com>
2016-02-27 20:56:11 +0800

    add example

--- ConsoleSpinner/Program.cs
+++ ConsoleSpinner/Program.cs
@@ -8,8 +8,26 @@ namespace ConsoleSpinner
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the program entry point.
+        /// </summary>
+        /// <param name="args">An array of <see cref="T:System.String"/> containing command line parameters.</param>
+        private static void Main(string[] args)
         {
+            MainAsync(args).Wait();
+        }
+
+        /// <summary>
+        /// The async main method body.
+        /// </summary>
+        /// <param name="args">An array of <see cref="T:System.String"/> containing command line parameters.</param>
+        /// <returns>Waitable <see cref="T:System.Threading.Tasks.Task"/></returns>
+        private static async Task MainAsync(string[] args)
+        {
+            using (new ConsoleSpinner())
+            {
+                await Task.Delay(2000);
+            }
         }
     }
 }
